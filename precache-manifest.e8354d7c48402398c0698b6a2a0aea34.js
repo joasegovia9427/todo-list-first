7ed9ff000ae318ed75bf24d563f9e157ca31439a
Joaquin Segovia <joasegovia9427@gmail.com>
2022-08-05 22:07:04 -0300

    Updates

---
+++ precache-manifest.e8354d7c48402398c0698b6a2a0aea34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "995489807e04390c7ed6",
+    "url": "/todo-list-first/static/css/main.a9d838e3.chunk.css"
+  },
+  {
+    "revision": "995489807e04390c7ed6",
+    "url": "/todo-list-first/static/js/main.99548980.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/todo-list-first/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "04821a7749e958ca4550",
+    "url": "/todo-list-first/static/js/runtime~main.04821a77.js"
+  },
+  {
+    "revision": "a6fefa0f3974ed6721cf772a7523ce4b",
+    "url": "/todo-list-first/index.html"
+  }
+];
